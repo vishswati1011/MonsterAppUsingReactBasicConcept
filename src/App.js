3c4971566b42f4adb5d21d87dc8f993236df89d6
unknown <vishswati1011>
2022-01-21 22:03:20 +0530

    lifecycle branch

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import React from 'react';
 import './App.css';
-import { SearchBox } from './components/search-box/search-box.component'
-import { CardList } from './components/card-list/card-list.component'
+import Lifecycle from './Lifecycle/Lifecycle'
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      monsters: [],
-      searchFeild: ''
+      showChild:true,
+      text: '',
     };
     // this.handleChange= this.handleChange.bind(this);
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
-  }
+  
 
-  handleChange = e =>{
-    this.setState({ searchFeild: e.target.value })
-  }
   render() {
-    const { monsters, searchFeild } = this.state;
-    const filteredMoneters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchFeild.toLowerCase())
-    );
     return (
       <div className="App">
-        {/* <input type='search'
-          onChange={e => this.setState({ searchFeild: e.target.value })}
-          placeholder='search monster' /> */}
-          <h1>MONSTER ROLODEX</h1>
-        <SearchBox
-          placeholder="search monster"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMoneters}>
-        </CardList>
+          <h1>Lifecycles Component</h1>
+          <button onClick={()=>
+            this.setState(state=>({
+            showChild:!state.showChild
+            }))
+            }
+          >
+            Toggle Lifecycles
+          </button>
+          <button 
+          onClick={()=>
+            this.setState(state=>({
+            text:state.text+'_hello'
+            }))
+            }
+          >
+            Update Text
+          </button>
+          <br/>
+         {this.state.showChild?<Lifecycle text={this.state.text} />:null}
       </div>
     );
   }
