0c34fd9fa302e4f61576ebf58c20ed0018fa6a70
unknown <vishswati1011>
2022-01-21 13:45:20 +0530

    first commit react basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component'
+import { CardList } from './components/card-list/card-list.component'
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchFeild: ''
+    };
+    // this.handleChange= this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = e =>{
+    this.setState({ searchFeild: e.target.value })
+  }
+  render() {
+    const { monsters, searchFeild } = this.state;
+    const filteredMoneters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchFeild.toLowerCase())
+    );
+    return (
+      <div className="App">
+        {/* <input type='search'
+          onChange={e => this.setState({ searchFeild: e.target.value })}
+          placeholder='search monster' /> */}
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMoneters}>
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
